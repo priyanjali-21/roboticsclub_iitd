bf2cc0e72f42113dd48a5f797fed1771ced72450
priyanjali-21 <160835556+priyanjali-21@users.noreply.github.com>
2024-12-24 00:55:39 +0530

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,9 @@
+// Smooth Scrolling for Navigation
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
